1b6f601e28b7050429b583f02a45699ec265286f
joon fly <joon@notique.co>
2019-06-04 23:05:38 +0900

    last commit

--- src/index.js
+++ src/index.js
@@ -7,25 +7,36 @@ import ApprovalCard from "./ApprovalCard";
 const App = () => {
   return (
     <div className='ui container comments'>
-      <ApprovalCard />
-      <CommentDetail
-        author='Sam'
-        timeAgo='Today at 4:45pm'
-        comment='great'
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author='Alex'
-        timeAgo='Today at 2:00am'
-        comment='not so great'
-        avatar={faker.image.avatar()}
-      />
-      <CommentDetail
-        author='Jane'
-        timeAgo='Yesterday at 10:00pm'
-        comment='actually couldnt care less'
-        avatar={faker.image.avatar()}
-      />
+      <ApprovalCard>
+        <div>
+          <h4>Warning!</h4>
+          Are you sure you want to do this?
+        </div>
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author='Sam'
+          timeAgo='Today at 4:45pm'
+          comment='great'
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author='Alex'
+          timeAgo='Today at 2:00am'
+          comment='not so great'
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
+      <ApprovalCard>
+        <CommentDetail
+          author='Jane'
+          timeAgo='Yesterday at 10:00pm'
+          comment='actually couldnt care less'
+          avatar={faker.image.avatar()}
+        />
+      </ApprovalCard>
     </div>
   );
 };
